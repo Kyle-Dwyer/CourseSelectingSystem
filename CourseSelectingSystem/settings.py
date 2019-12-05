59de6174a2d389c88b59632b6f95bb9deb7cf7c2
yongli0510 <17302010031@fudan.edu.cn>
2019-12-05 18:36:30 +0800

    项目基本架构构建完成

--- CourseSelectingSystem/settings.py
+++ CourseSelectingSystem/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp'
+    'administrator',
+    'student',
+    'teacher'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
